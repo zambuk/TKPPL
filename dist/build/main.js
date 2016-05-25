26e87c5c13f5d6e57fa4993eb8410bc47b81e295
Willy Laurents <willy.lau123@yahoo.co.id>
2016-05-25 14:26:44 +0700

    message

--- dist/build/main.js
+++ dist/build/main.js
@@ -1,4 +1,40 @@
-function myFunction()
+function myFunctionLife(param)
 {
-    life;   
+    if(param==null)
+    {
+        life = 3;
+    }
+    else
+    {
+        life = 3-param;
+    }
+    
+    console.log(life);
+    var life0 = ["emptylove.png","emptylove.png","emptylove.png"];
+    var life1 = ["fulllove.png","emptylove.png","emptylove.png"];
+    var life2 = ["fulllove.png","fulllove.png","emptylove.png"];
+    var life3 = ["fulllove.png","fulllove.png","fulllove.png"];
+    var i;
+    var templife="";
+    for(i = 0;i<3;i++)
+    {
+        if(life==0)
+        {
+            templife += "<img src='images/"+life0[i]+"'"+">"+"  ";
+        }
+        else if(life == 1)
+        {
+            templife += "<img src='images/"+life1[i]+"'"+">"+"  ";
+        }
+        else if(life==2)
+        {
+            templife += "<img src='images/"+life2[i]+"'"+">"+"  ";
+        }
+        else if(life==3)
+        {
+            templife += "<img src='images/"+life3[i]+"'"+">"+"  ";
+        }
+
+    }
+    document.getElementById("life").innerHTML = templife;
 }
